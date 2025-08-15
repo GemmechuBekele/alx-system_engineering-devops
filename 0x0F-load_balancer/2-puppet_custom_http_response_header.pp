1090e2a01f6e7ad3bea711714bbe32e79ce71d8c
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-08-15 07:20:21 +0300

    Fix 2-puppet_custom_http_response_header.pp file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,22 @@
-# 2-puppet_custom_http_response_header.pp
+# Configures Nginx to add a custom X-Served-By header with the hostname
 
 package { 'nginx':
   ensure => installed,
 }
 
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => Package['nginx'],
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
 }
 
-file { '/etc/nginx/conf.d/custom_header.conf':
+# Create Nginx configuration file to add custom header
+file { '/etc/nginx/conf.d/custom_headers.conf':
   ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    add_header X-Served-By \$hostname;
-
-    root /var/www/html;
-    index index.html index.htm;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-",
-  notify  => Service['nginx'],
+  content => "add_header X-Served-By \$hostname;\n",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   require => Package['nginx'],
+  notify  => Service['nginx'],
 }
